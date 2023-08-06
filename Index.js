623494dcaaaba4c0070a649d5f64062dbce91edc
Ravi Pandey <67542571+Pandey9800@users.noreply.github.com>
2023-08-06 15:14:32 +0530

    Initial commit

---
+++ Index.js
@@ -0,0 +1,26 @@
+// console.log("Wassup Nigga")
+
+// function add(a, b, c)
+// {
+//     return a + b + c;
+// }
+
+// let c = add(44, 11, 45)
+// console.log(c)
+
+// let b = 'Yolo'
+// let a = 56
+// console.log(b + a)
+
+// const words = {
+//     fir: "Red",
+//     sec: "Blue",
+//     thr: "Green",
+//     fou: "Yellow",
+//     fiv: "Pink"
+// }
+
+// let a = prompt("How old are you Bitch?")
+
+// console.log(a)
+
